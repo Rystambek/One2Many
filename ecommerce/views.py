b29a808322ce47216776f093d8d73db390628cab
Rystambek <rustambekjumaqulov@gmail.com>
2023-06-01 19:23:19 +0500

    GET /companies

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.http import HttpRequest,JsonResponse
+from .models import Company,Product
+import json
 
-# Create your views here.
+def to_company(company:Company) -> dict:
+    return {
+        'id' : company.pk,
+        'name': company.name,
+        'website':company.website
+    }
+
+def to_product(product:Product) -> dict:
+    return {
+        'id':product.pk,
+        'company_id' : product.company.pk,
+        'name' : product.name,
+        'price' : product.price
+    }
+
+def get_company(request:HttpRequest) -> JsonResponse:
+    if request.method == 'GET':
+        companies = Company.objects.all()
+        result = [to_company(company) for company in companies]
+
+        return JsonResponse(result,safe=False)
