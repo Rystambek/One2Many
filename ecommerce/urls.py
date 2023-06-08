f6c80c41cd7779ad228e8ec5d40126fcfb3de695
Rystambek <rustambekjumaqulov@gmail.com>
2023-06-08 19:08:03 +0500

    products<int:company_id>/products/<int:id>

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('',CompanyView.as_view()),
     path('<int:id>',CompanyIdView.as_view()),
     path("<int:id>/products",ProductView.as_view()),
-    path('<int:company_id>/produts/<int:id>',ProductIdView.as_view())
+    path('<int:company_id>/products/<int:id>',ProductIdView.as_view())
 ]
